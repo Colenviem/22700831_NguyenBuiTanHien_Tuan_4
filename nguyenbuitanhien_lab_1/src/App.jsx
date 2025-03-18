93f3f501fb326e843a761724f450684d9bde8ca8
Colenviem <hiennguyenbuitan@gmail.com>
2025-03-18 20:35:47 +0700

    Update week 4

--- nguyenbuitanhien_lab_1/src/App.jsx
+++ nguyenbuitanhien_lab_1/src/App.jsx
@@ -1,26 +1,19 @@
 import { useState, useEffect } from 'react';  
-import axios from "axios";
 import './App.css';
-import Items from './components/Item.jsx';
+import Header from './components/Header.jsx';
+import Navbar from './components/Navbar.jsx';
+import Body from './components/Body.jsx';
+import Footer from './components/footer.jsx';
 
 function App() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const fetchItems = async () => {
-      try {
-        const res = await axios.get("https://67c83ca60acf98d0708589e8.mockapi.io/api/data/items");
-        setItems(res.data);
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    fetchItems();
-  }, []);
+  
 
   return (
     <>
-      <Items items={items} />  
+      <Header></Header>
+      <Navbar></Navbar>
+      <Body></Body>
+      <Footer></Footer>
     </>
   );
 }
